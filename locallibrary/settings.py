e03126c91ef0c1fb4abac584052e68377e60ed02
Andrey Pak <andrey.g.park@gmail.com>
2018-08-06 04:03:11 +0300

    Added files and changes required for deployment to heroku

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'dzx16qy)qz-537**7)#zei4_9#0rn49ku9pfn-d-si^0y74_s='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
